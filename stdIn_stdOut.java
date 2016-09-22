ac6fa49e79d11cea271f4bb177d3e2e73e5d0c2a
Naveen Kumar BT <btnaveenkumar@gmail.com>
2016-09-22 11:25:08 +0530

    Create stdIn_stdOut.java
    https://www.hackerrank.com/challenges/java-stdin-stdout
    
    Input Format
    
    There are three lines of input:
    
    The first line contains an integer.
    The second line contains a double.
    The third line contains a String.
    Output Format
    
    There are three lines of output:
    
    On the first line, print String: followed by the unaltered String read from stdin.
    On the second line, print Double: followed by the unaltered double read from stdin.
    On the third line, print Int: followed by the unaltered integer read from stdin.
    To make the problem easier, a portion of the code is already provided in the editor.
    
    Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens; because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() will be reading the remainder of the integer line (which is empty).
    
    Sample Input
    
    42
    3.1415
    Welcome to HackerRank's Java tutorials!
    Sample Output
    
    String: Welcome to HackerRank's Java tutorials!
    Double: 3.1415
    Int: 42

---
+++ stdIn_stdOut.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //scan.useDelimiter("\\n");
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); // Very much needed to avoid printing nextline; else use a different scanner.
+        String s = scan.nextLine();     
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
